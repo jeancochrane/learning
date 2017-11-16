0a0a2f1d31abe40c123458fca53b2eb85681c7c7
Jean Cochrane <jean@jeancochrane.com>
2017-11-15 20:02:37 -0600

    Finish exercise 1.19 in SICP
    Signed-off-by: Jean Cochrane <jean@jeancochrane.com>

---
+++ sicp/exercises/1.2/code/fast-mult.scm
@@ -0,0 +1,14 @@
+(define (double n)
+  (+ n n))
+
+(define (halve n)
+  (/ n 2))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (fast-mult a b)
+  ;; Multiply two numbers in O(log n) time
+  (cond ((= b 0) 0)
+        ((even? b) (double (fast-mult a (halve b))))
+        (else (+ a (fast-mult a (- b 1))))))
