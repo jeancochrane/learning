a193ce1a9fa94cdcee169ddef39401b742d33dc7
Jean Cochrane <jean@jeancochrane.com>
2017-11-24 09:28:27 -0800

    Clean up pascal's triangle code

--- sicp/exercises/1.2/code/pascal.scm
+++ sicp/exercises/1.2/code/pascal.scm
@@ -1,29 +1,29 @@
 (define (pascal row col)
   ;;; Calculate a value in Pascal's triangle for a given row and col
-  (cond ((or (= col 1) (= row col)) 1)
-        ((or (< col 1) (> col row)) 0)
-        (else (+ (pascal (- row 1) (- col 1))
-                 (pascal (- row 1) col)))))
+  (cond ((or (= col 1) (= row col)) 1)  ; Edges of the triangle
+        ((or (< col 1) (> col row)) 0)  ; Ignore values beyond triangle bounds
+        (else (+ (pascal (- row 1) (- col 1))  ; Preceding value on the left
+                 (pascal (- row 1) col)))))    ; Preceding value on the right
 
-(define (coef n)
+(define (binom-coef n)
   ;;; Display the binomial coefficients for `n`
   (define (pascal-row row col)
     (if (= row col)
         (write-to-string (pascal row col))
         (string-append (write-to-string (pascal row col))
-                        " "
-                        (pascal-row row (+ col 1)))))
+                       " "
+                       (pascal-row row (+ col 1)))))
   (pascal-row n 1))
 
 (define (pascals-triangle n)
   ;;; Display an inverted version of Pascal's triangle starting
   ;;; at row `n`
-  (define (build-triangle n)
-    (if (= n 1)
-        (coef n)
-        (string-append (coef n) "\n" (build-triangle (- n 1)))))
-  (display (build-triangle n)))
-
-
+  (define (build-triangle n count)
+    (if (= count n)
+        (binom-coef count)
+        (string-append (binom-coef count)
+                       "\n"
+                       (build-triangle n (+ count 1)))))
+  (display (build-triangle n 1)))
 
 
